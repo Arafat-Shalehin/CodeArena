55e1f064bad45592654ee6115bf240b48e3c8b60
Shahnawas Adeel <64582744+mdadeel@users.noreply.github.com>
2026-02-17 21:26:13 +0600

    fix(api): force dynamic rendering for users route

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -2,12 +2,14 @@ import dbConnect from "@/lib/mongodb";
 import { createUser, fetchUsers } from "@/controllers/user.controller";
 import { asyncHandler } from "@/lib/asyncHandler";
 
+export const dynamic = 'force-dynamic';
+
 export const POST = asyncHandler(async (req) => {
   await dbConnect();
   return createUser(req);
 });
 
-export const GET = asyncHandler(async(req) => {
+export const GET = asyncHandler(async (req) => {
   await dbConnect();
   return fetchUsers();
 })
